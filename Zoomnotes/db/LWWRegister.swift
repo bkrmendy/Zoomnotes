e9fe8f494e22f7c1c5599e44ea5cc737cbd52645
Bertalan Körmendy <korber314@gmail.com>
2020-12-22 12:02:36 +0100

    addd crdt types

---
+++ Zoomnotes/db/LWWRegister.swift
@@ -0,0 +1,16 @@
+//
+//  LWWRegister.swift
+//  Zoomnotes
+//
+//  Created by Berci on 2020. 12. 22..
+//  Copyright © 2020. Berci. All rights reserved.
+//
+
+import Foundation
+
+protocol LWWRegister {
+    associatedtype Value
+    func `assert`(entity: UUID, value: Value)
+    func retract(entity: UUID, value: Value)
+    func get(for entity: UUID) -> Value?
+}
